cd39500eaa5dd98ce37d3a2af782a8f914aedf16
CodingEmil <116643407+CodingEmil@users.noreply.github.com>
2025-12-13 11:09:30 +0100

    Removed 'get_session' from the package exports in __init__.py

--- src/database/__init__.py
+++ src/database/__init__.py
@@ -6,7 +6,6 @@ from .models import Base, Transaction # Importiere die ORM-Modelle aus models.py
 __all__ = [ #Definiert, welche Objekte beim Importieren des Packages sichtbar sind, diese kommen aus den importierten Modulen, also connection.py und models.py
     'engine', #Verbindungsobjekt zur Datenbank
     'SessionLocal', #Session Factory für Datenbankoperationen; genutzt um Sitzungen zu erstellen und zu verwalten
-    'get_session', #Funktion zum Abrufen einer neuen Datenbank-Session
     'init_db', #Funktion zur Initialisierung der Datenbank und Erstellung der Tabellen
     'Base', #Basisklasse für alle ORM-Modelle 
     'Transaction' #ORM-Modell für Transaktionen
